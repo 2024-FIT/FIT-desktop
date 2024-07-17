e3e199f00f58344699cfa3d6a6f1b3aebb1d93f2
je355 <chaeeehui@gmail.com>
2024-07-18 00:57:12 +0900

    ff

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import * as S from '@/components/calendar/index.style';
 
 import TodayMenuImg from "@/assets/today/todaymymeal.svg"
