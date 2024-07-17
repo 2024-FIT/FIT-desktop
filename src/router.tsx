a2085f08bac33b145d26a6b854982e219dbbb31a
joungminsung <joungminsung080830@gmail.com>
2024-07-17 16:10:39 +0900

    프로필 페이지 추가

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Onbording from "@/pages/onbording/index";
 import Info from '@/pages/info/index'
 import MainPage from "@/pages/main/index"
+import Profile from '@/pages/profile/index';
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
         <Route path="/onbording" element={<Onbording />} />
         <Route path="/info" element={<Info />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
