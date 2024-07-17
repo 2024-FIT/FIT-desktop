83be3fc3145090ec1aff39f66b9ac37e590556b9
joungminsung <joungminsung080830@gmail.com>
2024-07-17 20:46:58 +0900

    건강상태 좌측 완료

--- src/pages/profile/index.style.ts
+++ src/pages/profile/index.style.ts
@@ -15,14 +15,21 @@ export const mainbox = styled.div`
     right:7%;
 `;
 
+export const leftbox = styled.div`
+    height:100%;
+    width:40%;
+    position:relative;
+    float: left;
+`;
+
 export const character = styled.div`
-    width: 346px;
-    height: 289px;
+    height: 40%;
+    width: 100%;
     flex-shrink: 0;
-    background: url("https://velog.velcdn.com/images/jva102/post/236fabe9-5bc7-4790-93c0-3c473c5cd393/image.png")  50% / cover no-repeat;
+    background: url("https://velog.velcdn.com/images/jva102/post/236fabe9-5bc7-4790-93c0-3c473c5cd393/image.png")  50% / contain no-repeat;
     position: absolute;
-    transform:translate(0,-50%);
-    left: 10%;
+    transform:translate(-50%,-50%);
+    left: 50%;
     top:50%;
 `;
 
@@ -33,5 +40,99 @@ export const back = styled.img`
     fill: #A2CC71;
     position: absolute;
     top: 2%;
-    left: 1%;
+    left: 2%;
+`;
+
+export const title = styled.div`
+    width:190px;
+    position:absolute;
+    transform:translate(-50%);
+    top:15%;
+    left:50%;
+`;
+
+export const name = styled.div`
+    width: 72px;
+    height: 33px;
+    flex-shrink: 0;
+    border-radius: 50px;
+    background: #A2CC71;
+
+    position:absolute;
+    left:0;
+    top:15%;
+
+    text-align:center;
+    line-height: 33px;
+    color:white;
+    color: var(--Sub-White, #FFF);
+    font-family: Pretendard;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 700;
+`;
+
+export const nameText = styled.span`
+    color: #5B5B5B;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 21px;
+    font-style: normal;
+    font-weight: 600;
+    position: absolute;
+    line-height:33px;
+    right: 0;
+`;
+
+export const BMIbar = styled.div`
+    width:70%;
+    height:35px;
+    background: linear-gradient(90deg,#A8DAFF 20%,#AAF27E 20%,#AAF27E 40%,#EBFF6F 40%,#EBFF6F 60%,#FFDF6F 60%,#FFDF6F 80%,#FF6F6F 80%);
+    position: absolute;
+    transform:translate(-50%);
+    left: 50%;
+    top:80%;
+    border-radius:10px;
+`;
+
+export const underarrow = styled.img`
+    position:absolute;
+    top:78%;
+    left:82%;
+`;
+
+export const rightbox = styled.div`
+    float:right;
+    height:100%;
+    width:60%;
+    background-color:red;
+    position:relative;
+`;
+
+export const editbutton = styled.div`
+        width: 401px;
+    height: 54px;
+    border-radius: var(--Large, 12px);
+    background: #A2CC71;
+    border: none;
+    position: absolute;
+    transform:translate(-50%);
+    top:80%;
+    left:50%;
+
+    color: var(--Sub-White, #FFF);
+
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 54px;
+    cursor:pointer;
+
+
+    &:hover{
+        color:lightgray;
+        background-color: #8AAE60;
+    }
 `;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,9 +5,13 @@ import * as S from '@/pages/profile/index.style';
 
 import Side from "@/components/navbar/index"
 import arrow from "@/assets/arrow.svg"
-
+import underarrow from "@/assets/profile/underarrow.svg"
 
 const Index = () => {
+    let bmi = 30;
+    const loadbmi = () =>{
+        
+    }
     return(
         <div style={{
             width: '100vw',
@@ -17,9 +21,19 @@ const Index = () => {
         }}>
             <Side></Side>
             <S.mainbox>
-                <S.back src = {arrow}></S.back>
-                <S.name></S.name>
-                <S.character></S.character>
+                <S.leftbox>
+                        <S.back src = {arrow}></S.back>
+                    <S.title>
+                        <S.name>김민규</S.name><S.nameText>님의 건강상태</S.nameText>
+                    </S.title>
+                    <S.character></S.character>
+                    <S.BMIbar></S.BMIbar>
+                    <S.underarrow src={underarrow}></S.underarrow>
+                </S.leftbox>
+                <S.rightbox>
+                    
+                    <S.editbutton>정보 수정 하기</S.editbutton>
+                </S.rightbox>
             </S.mainbox>
         </div>
         
